6ab5fd2f84831579285fc1403d5c289217e1c7aa
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-06-27 23:08:35 +0530

    Recipe Creation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,35 @@ class RecipesController < ApplicationController
     @user = User.find(params[:id])
     @recipe = @user.recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(
+      user_id: current_user.id, 
+      name:recipe_params[:name],
+      description:recipe_params[:description],
+      cooking_time:recipe_params[:cooking_time],
+      preparation_time:recipe_params[:preparation_time]
+    )
+
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to recipe_path(@recipe), notice: 'Food was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :cooking_time, :preparation_time)
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,6 @@ class Recipe < ApplicationRecord
   # validation
   validates :name, presence:true
   validates :description, presence:true
-  validates :preparation_time, numericality:true
-  validates :cooking_time, numericality:true
+  validates :preparation_time, numericality:{greater_than:0}
+  validates :cooking_time, numericality:{greater_than:0}
 end
