f0e35c2640855a7b759f911055b255250a22a458
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-27 09:17:36 +0000

    fix ruby linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def nav_active(page)
-    return 'active' if current_page?(page) || request.original_fullpath.start_with?(page + '/')
+    return 'active' if current_page?(page) || request.original_fullpath.start_with?("#{page}/")
   end
 end
