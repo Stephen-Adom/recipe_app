d0551b3c15bad93b4669f3e0d2e8dbea10962399
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-06-28 14:50:13 +0000

    add shopping list for each recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,4 +9,22 @@ class Recipe < ApplicationRecord
   validates :description, presence: true
   validates :preparation_time, numericality: { greater_than: 0 }
   validates :cooking_time, numericality: { greater_than: 0 }
+
+  def shopping_list(current_user)
+    shopping_list = []
+    user_food_list = Food.where(user_id: current_user.id).all
+
+    recipe_foods.each do |recipe_food|
+      food = user_food_list.find_by(id: recipe_food.food_id)
+
+      next unless !user_food_list.include?(recipe_food.food) || recipe_food.quantity > food.quantity
+
+      shopping_list << {
+        **food.attributes,
+        quantity: recipe_food.quantity - food.quantity
+      }
+    end
+
+    shopping_list
+  end
 end
