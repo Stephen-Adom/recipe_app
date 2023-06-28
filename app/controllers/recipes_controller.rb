d1f5183975cd1d786c03f99afcc10718b2d76ef5
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-06-28 13:54:34 +0530

    Rubocop Fix

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,27 +3,22 @@ class RecipesController < ApplicationController
     @recipes = Recipe.includes(:user).all
   end
 
-  def show
-    @user = User.find(params[:id])
-    @recipe = @user.recipe.find(params[:id])
-  end
-
   def new
     @recipe = Recipe.new
   end
 
   def create
     @recipe = Recipe.new(
-      user_id: current_user.id, 
-      name:recipe_params[:name],
-      description:recipe_params[:description],
-      cooking_time:recipe_params[:cooking_time],
-      preparation_time:recipe_params[:preparation_time]
+      user_id: current_user.id,
+      name: recipe_params[:name],
+      description: recipe_params[:description],
+      cooking_time: recipe_params[:cooking_time],
+      preparation_time: recipe_params[:preparation_time]
     )
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to recipe_path(@recipe), notice: 'Food was successfully created.' }
+        format.html { redirect_to recipe_path(@recipe), notice: 'Recipe was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,10 @@
 class Recipe < ApplicationRecord
   # association
   belongs_to :user
-  
+
   # validation
-  validates :name, presence:true
-  validates :description, presence:true
-  validates :preparation_time, numericality:{greater_than:0}
-  validates :cooking_time, numericality:{greater_than:0}
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :preparation_time, numericality: { greater_than: 0 }
+  validates :cooking_time, numericality: { greater_than: 0 }
 end
