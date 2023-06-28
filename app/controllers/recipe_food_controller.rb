3132e4fe157f21745f9d3f402a39e8826af06835
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-06-28 19:17:09 +0530

    creation

--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -1,2 +1,17 @@
 class RecipeFoodController < ApplicationController
+    def create
+        @recipe_food = Recipe_food.new(params[:recipe_food])
+        if @recipe_food.save
+          flash[:success] = "Object added successfully"
+        else
+          flash[:error] = "Something went wrong"
+        end
+    end
+
+    private
+
+    def recipe_food_params
+        params.require(:recipe_food).permit(:quantity)
+    end
+    
 end
