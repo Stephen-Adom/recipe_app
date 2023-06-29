a24c9ee609a0caabb5c8b5485e78e636cd6eb358
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-06-29 15:53:12 +0530

    Food Spec

--- spec/system/foods_spec.rb
+++ spec/system/foods_spec.rb
@@ -2,8 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Foods", type: :system do
   before do
-    driven_by(:rack_test)
+    @user_one = if User.find_by(email: 'Adarsh.pathak@example.com').present?
+                  User.find_by(email: 'Adarsh.pathak@example.com')
+                else
+                  User.create(name: 'Adarsh Pathak', email: 'Adarsh.pathak@example.com', password: '123456',
+                              confirmed_at: DateTime.now)
+                end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'should have "Food List" and ADD button' do 
+    visit root_path
+    fill_in 'Email', with: 'Adarsh.pathak@example.com'
+    fill_in 'Password', with: '123456'
+    click_button 'Log In'
+    click_link 'Food List'
+    expect(page).to have_content('Food List')
+    expect(page).to have_content('Add Food')
+  end
 end
