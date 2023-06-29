54be60e4b81bd6b2010c723a53c01dcc873ceff1
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-06-29 12:13:15 +0530

    Fix Confirmation

--- spec/system/log_in_spec.rb
+++ spec/system/log_in_spec.rb
@@ -16,4 +16,14 @@ RSpec.describe '/users/', type: :system do
     click_button 'Log In'
     expect(page).to have_content 'Signed in successfully.'
   end
+
+  it "New user can sign up" do 
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Name', with: 'Stephen Adom'
+    fill_in 'Email', with: 'Stephen.Adom@example.com'
+    fill_in 'Password', with: 123456
+    fill_in 'Password confirmation', with: 123456
+    click_button 'Sign up'
+  end
 end
